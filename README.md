ee1853bbc30304ec7d2b7893589bce53a65f8672
Ryan Rymarczyk <rymarczy@gmail.com>
2022-07-29 07:44:24 -0400

    update host IP on existing config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Home Assistant with X10
 
-This repository is meant to bootstrap a Home Assistant docker container with X10 support via [mochad](https://github.com/bjonica/mochad). 
+This repository is meant to build and deploy a Home Assistant docker container with X10 support via [mochad](https://github.com/bjonica/mochad). 
 
 A simple web-ui configuration text editor is also installed via CausticLab's [hass-configurator-docker](https://github.com/CausticLab/hass-configurator-docker). 
 
@@ -20,18 +20,28 @@ Clone this repository:
 ```
 git clone git@github.com:rymarczy/ha-x10-docker.git
 ```
-Change directory and call docker-compose to launch containers in the background:
+Inside of cloned directly, update `HOST_IP` environmental variable in `docker-compose.yml` to the local IP address of the machine that will be running home assistant. It is set to `127.0.0.1` by default. This is required for i-frame of the web-ui configuration editor to run correctly. 
 ```
-cd ha-x10-docker/
-
+  environment:
+    - HOST_IP=127.0.0.1
+```
+Then, build and launch docker containers. `-d` option launches the containers in the background. 
+```
 docker-compose up -d
 ```
+`docker ps` can be used to verify the containers are running correctly.
+```
+CONTAINER ID   IMAGE                                        COMMAND                  CREATED         STATUS          NAMES
+f637b5ced73d   ha-x10-docker_homeassistant                  "/usr/local/sbin/doc…"   16 hours ago    Up 11 seconds   homeassistant
+3b6522c5c3a8   ha-x10-docker_mochad                         "./mochad -d"            16 hours ago    Up 11 seconds   mochad
+c91c7246a855   causticlab/hass-configurator-docker:latest   "/app/run.sh"            16 hours ago    Up 11 seconds   config
+```
 
-Home assistant instance should now be available from [http://localhost:8123](http://localhost:8123) in your web browser.
+Home assistant instance should now be available from `http://<HOST_IP>:8123` in your web browser.
 
 # Configuration
 
-The web-ui configuration should be available by clicking [**Config Editor**](http://localhost:8123/configurator) on the left hand toolbar. 
+The web-ui configuration should be available by clicking **Config Editor** on the left hand toolbar. 
 
 Mochad X10 settings can not be configured through the UI.
 
@@ -47,8 +57,8 @@ light:
         comm_type: pl
 
 ```
-Detailed configuraion instructions are available on the [home assistant](https://www.home-assistant.io/integrations/mochad/) web site. 
+Detailed configuration instructions are available on the [home assistant](https://www.home-assistant.io/integrations/mochad/) web site. 
 
 Restart home assistant after saving any changes to `configuration.yaml` to see updates in UI.  
 
-*Developer Tools* > *Check Configuration* > *Restart*
+- *Developer Tools* > *Check Configuration (Make sure check succeeds)* > *Restart*
--- scripts/my-init.py
+++ scripts/my-init.py
@@ -1,48 +1,77 @@
+import os
 import pathlib
+import re
 
-init_check = pathlib.Path('/config/init-check')
-if init_check.is_file():
-    quit()
-
-init_text = (
-"\n"
-"# Loads default set of integrations. Do not remove.\n"
-"default_config:\n"
-"\n"
-"# Text to speech\n"
-"tts:\n"
-"  - platform: google_translate\n"
-"\n"
-"automation: !include automations.yaml\n"
-"script: !include scripts.yaml\n"
-"scene: !include scenes.yaml\n"
-"\n"
-"panel_iframe:\n"
-"  configurator:\n"
-"    title: Config Editor\n"
-"    icon: mdi:wrench\n"
-"    url: http://localhost:8124\n"
-"\n"
-"mochad:\n"
-"  host: mochad\n"
-"  port: 1099\n"
-"\n"
-"light:\n"
-"  - platform: mochad\n"
-"    devices:\n"
-"      - address: a1\n"
-"        name: test_light\n"
-"        comm_type: pl\n"
-)
-with open('/config/configuration.yaml', 'a') as f:
-    f.write(init_text)
-with open('/config/automations.yaml', 'a') as f:
-    pass
-with open('/config/scripts.yaml', 'a') as f:
-    pass
-with open('/config/scenes.yaml', 'a') as f:
-    pass
+def create_configs(host_ip:str, root_folder:str):
+    init_text = (
+    "\n"
+    "# Loads default set of integrations. Do not remove.\n"
+    "default_config:\n"
+    "\n"
+    "# Text to speech\n"
+    "tts:\n"
+    "  - platform: google_translate\n"
+    "\n"
+    "automation: !include automations.yaml\n"
+    "script: !include scripts.yaml\n"
+    "scene: !include scenes.yaml\n"
+    "\n"
+    "panel_iframe:\n"
+    "  configurator:\n"
+    "    title: Config Editor\n"
+    "    icon: mdi:wrench\n"
+    f"    url: http://{host_ip}:8124\n"
+    "\n"
+    "mochad:\n"
+    "  host: mochad\n"
+    "  port: 1099\n"
+    "\n"
+    "light:\n"
+    "  - platform: mochad\n"
+    "    devices:\n"
+    "      - address: a1\n"
+    "        name: test_light\n"
+    "        comm_type: pl\n"
+    )
+    os.makedirs(root_folder, exist_ok=True)
+    with open(os.path.join(root_folder,'configuration.yaml'), 'w') as f:
+        f.write(init_text)
+    with open(os.path.join(root_folder,'automations.yaml'), 'w') as f:
+        pass
+    with open(os.path.join(root_folder,'scripts.yaml'), 'w') as f:
+        pass
+    with open(os.path.join(root_folder,'scenes.yaml'), 'w') as f:
+        pass
 
 
-with open('/config/init-check', 'a') as f:
-    pass
+def update_config(host_ip:str, root_folder:str):
+    rep_str = f"http://{host_ip}:8124"
+
+    with open(os.path.join(root_folder,'configuration.yaml'), 'r') as f:
+        file_text = f.read()
+
+    file_text = re.sub(r'http:\/\/.+:8124', rep_str, file_text)
+    
+    with open(os.path.join(root_folder,'configuration.yaml'), 'w') as f:
+        f.write(file_text)
+
+
+def main():
+    config_root_folder = '/config'
+    try:
+        HOST_IP = os.environ['HOST_IP']
+    except Exception as e:
+        HOST_IP = 'localhost'
+
+    print(f"Host IP is: {HOST_IP}", flush=True)
+
+    config_file = os.path.join(config_root_folder,'configuration.yaml')
+
+    if pathlib.Path(config_file).is_file():
+        update_config(HOST_IP, config_root_folder)
+    else:
+        create_configs(HOST_IP, config_root_folder)
+
+
+if __name__ == '__main__':
+    main()
