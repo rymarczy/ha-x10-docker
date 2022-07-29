9a3f6e6c80182627c949059c590af9a13bf60392
Ryan Rymarczyk <rymarczy@gmail.com>
2022-07-29 09:57:48 -0400

    add logging

--- scripts/my-init.py
+++ scripts/my-init.py
@@ -1,7 +1,10 @@
 import os
 import pathlib
 import re
+import logging
 
+logging.basicConfig(level=logging.INFO)
+ 
 def create_configs(host_ip:str, root_folder:str):
     init_text = (
     "\n"
@@ -34,26 +37,39 @@ def create_configs(host_ip:str, root_folder:str):
     "        comm_type: pl\n"
     )
     os.makedirs(root_folder, exist_ok=True)
-    with open(os.path.join(root_folder,'configuration.yaml'), 'w') as f:
+    config_file = os.path.join(root_folder,'configuration.yaml')
+    with open(config_file, 'w') as f:
         f.write(init_text)
-    with open(os.path.join(root_folder,'automations.yaml'), 'w') as f:
+    logging.info(f"{config_file} created with default initialization.")
+
+    auto_file = os.path.join(root_folder,'automations.yaml')
+    with open(auto_file, 'w') as f:
         pass
-    with open(os.path.join(root_folder,'scripts.yaml'), 'w') as f:
+    logging.info(f"Blank {auto_file} created.")
+
+    script_file = os.path.join(root_folder,'scripts.yaml')
+    with open(script_file, 'w') as f:
         pass
-    with open(os.path.join(root_folder,'scenes.yaml'), 'w') as f:
+    logging.info(f"Blank {script_file} created.")
+
+    scene_file = os.path.join(root_folder,'scenes.yaml')
+    with open(scene_file, 'w') as f:
         pass
+    logging.info(f"Blank {scene_file} created.")
 
 
 def update_config(host_ip:str, root_folder:str):
     rep_str = f"http://{host_ip}:8124"
+    config_file = os.path.join(root_folder,'configuration.yaml')
 
-    with open(os.path.join(root_folder,'configuration.yaml'), 'r') as f:
+    with open(config_file, 'r') as f:
         file_text = f.read()
 
     file_text = re.sub(r'http:\/\/.+:8124', rep_str, file_text)
-    
-    with open(os.path.join(root_folder,'configuration.yaml'), 'w') as f:
+
+    with open(config_file, 'w') as f:
         f.write(file_text)
+    logging.info(f"Updated {config_file} to use {host_ip} for HOST_IP for config editor.")
 
 
 def main():
@@ -61,15 +77,18 @@ def main():
     try:
         HOST_IP = os.environ['HOST_IP']
     except Exception as e:
+        logging.error(f"No 'HOST_IP' environmental variable found, using 'localhost'.")
         HOST_IP = 'localhost'
 
-    print(f"Host IP is: {HOST_IP}", flush=True)
+    logging.info(f"Host IP is: {HOST_IP}")
 
     config_file = os.path.join(config_root_folder,'configuration.yaml')
 
     if pathlib.Path(config_file).is_file():
+        logging.info(f"Existing {config_file} found, updating config file...")
         update_config(HOST_IP, config_root_folder)
     else:
+        logging.info(f"Existing {config_file} NOT found, creating config files...")
         create_configs(HOST_IP, config_root_folder)
 
 
